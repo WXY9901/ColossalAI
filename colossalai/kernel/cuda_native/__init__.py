5e33ede09f3dae9f683868038d48dee772d1f8da
WXY9901 <wangxingyi_9901@163.com>
2023-07-10 16:04:04 +0800

    提交消息

--- colossalai/kernel/cuda_native/__init__.py
+++ colossalai/kernel/cuda_native/__init__.py
@@ -1,5 +1,11 @@
 from .layer_norm import MixedFusedLayerNorm as LayerNorm
 from .multihead_attention import MultiHeadAttention
 from .scaled_softmax import FusedScaleMaskSoftmax, ScaledUpperTriangMaskedSoftmax
+#from ._cpp_lib import _built_with_cuda
 
-__all__ = ['LayerNorm', 'MultiHeadAttention', 'FusedScaleMaskSoftmax', 'ScaledUpperTriangMaskedSoftmax']
+
+__all__ = [
+    'LayerNorm', 
+    'MultiHeadAttention', 
+    'FusedScaleMaskSoftmax', 
+    'ScaledUpperTriangMaskedSoftmax']
